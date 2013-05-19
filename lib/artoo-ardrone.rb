fb76605423a66528c29480ef7058050ba31a364a
deadprogrammer <ron@hybridgroup.com>
2013-05-19 15:26:54 -0700

    Need to require all adaptors and drivers in the gem

--- lib/artoo-ardrone.rb
+++ lib/artoo-ardrone.rb
@@ -1,3 +1,9 @@
 require 'lib/artoo/adaptors/ardrone'
+require 'lib/artoo/adaptors/ardrone_navigation'
+require 'lib/artoo/adaptors/ardrone_video'
+
 require 'lib/artoo/drivers/ardrone'
+require 'lib/artoo/drivers/ardrone_navigation'
+require 'lib/artoo/drivers/ardrone_video'
+
 require 'lib/artoo-ardrone/version'
