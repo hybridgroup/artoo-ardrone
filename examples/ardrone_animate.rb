1a86e1143e28baf007295360749e6ff3bf45615c
deadprogram <ron@hybridgroup.com>
2013-10-17 14:57:33 -0700

    Does a flip

--- examples/ardrone_animate.rb
+++ examples/ardrone_animate.rb
@@ -3,7 +3,7 @@ require 'artoo'
 connection :ardrone, :adaptor => :ardrone
 device :drone, :driver => :ardrone, :connection => :ardrone
 
-connection :navigation, :adaptor => :ardrone_navigation
+connection :navigation, :adaptor => :ardrone_navigation, :port => '192.168.1.1:5554'
 device :nav, :driver => :ardrone_navigation, :connection => :navigation
 
 work do
@@ -13,7 +13,7 @@ end
 
 def fly(*data)
   drone.take_off
-  after(10.seconds) { drone.animate(:turnaround, 3000) }
+  after(10.seconds) { drone.animate(:flip_ahead, 1000) }
   after(15.seconds) { drone.hover.land }
   after(20.seconds) { drone.stop }
 end
