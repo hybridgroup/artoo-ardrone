cca80f2d33e99802a56c228ea8ac492365a44ba3
zachrab <zachrab@gmail.com>
2013-10-23 13:39:38 -0700

    first test

--- test/drivers/ardrone_test.rb
+++ test/drivers/ardrone_test.rb
@@ -5,7 +5,14 @@ describe Artoo::Drivers::Ardrone do
   before do
     @device = mock('device')
     @driver = Artoo::Drivers::Ardrone.new(:parent => @device)
+    @connection = mock('connection')
+    @device.stubs(:connection).returns(@connection)
   end
 
-  it 'must do things'
+  it 'Ardrone#start with nav' do
+  	@connection.expects(:start).with(true)
+  	@device.expects(:start_with_nav).with(true)
+  	@driver.start(true)
+  end
+
 end
