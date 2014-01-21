8abeb5e93969054f260adcee196b2eff40e50299
zachrab <zachrab@gmail.com>
2014-01-20 21:22:31 -0800

    added more ardrone-driver-tests

--- test/drivers/ardrone_test.rb
+++ test/drivers/ardrone_test.rb
@@ -10,16 +10,48 @@ describe Artoo::Drivers::Ardrone do
   end
 
   describe 'Ardrone#start' do
-    it 'it starts without nav when nav is nil' do
+    it 'starts without nav when nav is nil' do
     	@connection.expects(:start).with(false)
-    	@device.expects(:event_topic_name).with("ready").returns("hi_ready")
+    	@device.expects(:event_topic_name).with("ready")
     	@driver.start(false)
     end
 
-    it 'it starts with nav when nav is not nil' do
+    it 'starts with nav when nav is not nil' do
     	@connection.expects(:start).with(false)
     	@driver.stubs(:start_with_nav).returns([])
     	@driver.start(true)
+    end
+  end
+
+  describe 'Ardrone#start_with_nav' do
+    before do
+      @nav = mock('navigation')
+    end
+
+    it 'starts with nav when theres no emergency landing' do
+      @nav.stubs(:emergency_landing?).returns(false)
+      @device.expects(:event_topic_name).with("ready")
+      @driver.start_with_nav(@nav)
+    end
+
+    it 'starts with nav when theres an emergency landing' do
+      @nav.stubs(:emergency_landing?).twice.returns(true)
+      @connection.expects(:disable_emergency)
+      @driver.start_with_nav(@nav)     
+    end
+  end
+
+  describe 'Ardrone#enable_emergency' do
+    it 'enables emergency' do
+      @connection.expects(:enable_emergency)
+      @driver.enable_emergency
+    end
+  end
+
+  describe 'Ardrone#disable_emergency' do
+    it 'disables emergency' do
+      @connection.expects(:disable_emergency)
+      @driver.disable_emergency
     end
   end
 end
